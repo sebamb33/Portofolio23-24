3320248052da7cfc99281a6b7b6da7887e8ad8c0
Sébastien33am <sebastien.ambona@agence-gw.com>
2024-10-26 18:23:12 +0200

    Add css into menu for mobile and little screen

--- components/menu.vue
+++ components/menu.vue
@@ -1,25 +1,35 @@
 <template>
-    <div class="menu_nav">
-        <div class="menu_link">
-            <NuxtLink to="/technology">Technologies</NuxtLink>
+    <div class="menu_nav" v-if="menuOpen">
+        <div class="menu_top" @click="closeMenu">
+            <img
+                src="https://img.icons8.com/ios-glyphs/30/000000/delete-sign.png"
+                heigh="50"
+            />
         </div>
-        <div class="menu_link">
-            <NuxtLink to="/experience">Experience</NuxtLink>
+        <div class="menu_router">
+            <div class="menu_link">
+                <NuxtLink to="/" @click="closeMenu"> Accueil </NuxtLink>
+            </div>
+            <div class="menu_link">
+                <NuxtLink to="/technology" @click="closeMenu"
+                    >Technologies</NuxtLink
+                >
+            </div>
+            <div class="menu_link" @click="closeMenu">
+                <NuxtLink to="/experience">Experience</NuxtLink>
+            </div>
         </div>
-    </div>
-    <div class="menu_logo">
-        <a
-            href="https://www.linkedin.com/in/s%C3%A9bastien-ambona-6b1b3a1b0/"
-            target="_blank"
-        >
-            <NuxtLink to="/">
-                <img :src="logoUrl" alt="Logo" width="70" height="70" />
-            </NuxtLink>
-            <img src="https://img.icons8.com/50/000000/linkedin.png" />
-        </a>
-        <a href="https://github.com/sebamb33" target="_blank">
-            <img src="https://img.icons8.com/50/000000/github--v1.png" />
-        </a>
+        <div class="menu_logo">
+            <a
+                href="https://www.linkedin.com/in/s%C3%A9bastien-ambona-6b1b3a1b0/"
+                target="_blank"
+            >
+                <img src="https://img.icons8.com/50/000000/linkedin.png" />
+            </a>
+            <a href="https://github.com/sebamb33" target="_blank">
+                <img src="https://img.icons8.com/50/000000/github--v1.png" />
+            </a>
+        </div>
     </div>
 </template>
 <script lang="ts">
@@ -27,7 +37,51 @@ export default {
     data() {
         return {
             logoUrl: "/assets/LogoPortofolio.svg",
+            menuOpen: true,
         };
+    },
+    methods: {
+        closeMenu() {
+            this.menuOpen = false;
+            this.$emit("closeMenu");
+        },
     },
 };
 </script>
+<style>
+.menu_nav {
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    padding: 0 20px;
+    margin-bottom: 20px;
+    position: absolute;
+    z-index: 300;
+    left: 0px;
+    top: 0px;
+    background: white;
+}
+.menu_top {
+    display: flex;
+    justify-content: flex-end;
+    padding: 30px;
+    margin-bottom: 50px;
+    align-items: center;
+}
+
+.menu_router {
+    height: 80vh;
+}
+.menu_link {
+    padding-bottom: 20px;
+    font-size: 50px;
+    font-weight: 800;
+}
+.menu_link a {
+    text-decoration: none;
+    color: black;
+    font-family: "Fira Code", monospace;
+}
+</style>
+```
--- layouts/navbar.vue
+++ layouts/navbar.vue
@@ -1,118 +1,132 @@
 <template>
-  <div class="Navbar">
-    <NuxtLink to="/">
-      <img :src="logoUrl" alt="Logo" width="70" height="70" />
-    </NuxtLink>
-    <div class="button-container" onclick="menu=!menu">
-      <div class="lien"><NuxtLink to="/technology">Technologies</NuxtLink></div>
-      <div class="lien"><NuxtLink to="/experience">Experience</NuxtLink></div>
-      <div class="lien"><NuxtLink to="/project">Projets</NuxtLink></div>
+    <div>
+        <div class="Navbar" v-if="!menuDisplay">
+            <NuxtLink to="/">
+                <img :src="logoUrl" alt="Logo" width="70" height="70" />
+            </NuxtLink>
+            <div class="button-container" onclick="menu=!menu">
+                <div class="lien">
+                    <NuxtLink to="/technology">Technologies</NuxtLink>
+                </div>
+                <div class="lien">
+                    <NuxtLink to="/experience">Experience</NuxtLink>
+                </div>
+                <div class="lien">
+                    <NuxtLink to="/project">Projets</NuxtLink>
+                </div>
+            </div>
+            <button
+                id="burger-icon"
+                class="burger-icon"
+                aria-label="Menu"
+                @click="menuDisplay = !menuDisplay"
+            >
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+        </div>
+        <Menu v-if="menuDisplay" @closeMenu="closeMenu" />
     </div>
-    <button id="burger-icon" class="burger-icon" aria-label="Menu">
-      <span></span>
-      <span></span>
-      <span></span>
-    </button>
-    <nav id="menu" class="menu" v-if="menu">
-      <ul>
-        <li><a href="#home">Home</a></li>
-        <li><a href="#about">About</a></li>
-        <li><a href="#services">Services</a></li>
-        <li><a href="#contact">Contact</a></li>
-      </ul>
-    </nav>
-  </div>
 </template>
 
 <style scoped>
 .button-container {
-  width: 30%;
-  margin-left: 60%;
-  display: flex;
-  flex-direction: row-reverse;
-  justify-content: space-between;
+    width: 30%;
+    margin-left: 60%;
+    display: flex;
+    flex-direction: row-reverse;
+    justify-content: space-between;
 }
 .lien {
-  background: none;
-  border: none;
-  font-size: 1em;
-  margin: 0;
-  padding: 0;
-  outline: none;
-  color: black;
-  transition: color 0.3s;
+    background: none;
+    border: none;
+    font-size: 1em;
+    margin: 0;
+    padding: 0;
+    outline: none;
+    color: black;
+    transition: color 0.3s;
 }
 .lien a {
-  text-decoration: none;
-  color: black;
+    text-decoration: none;
+    color: black;
 }
 
 .Navbar {
-  display: flex;
-  align-content: baseline;
-  position: relative;
-  width: 100%;
-  height: 15%;
-  margin-top: 1em;
+    display: flex;
+    align-content: baseline;
+    position: relative;
+    width: 100%;
+    height: 15%;
+    margin-top: 1em;
 }
 .burger-icon {
-  display: none;
-  background: none;
-  border: none;
+    display: none;
+    background: none;
+    border: none;
 }
 .burger-icon span {
-  display: block;
-  width: 30px;
-  height: 3px;
-  background: black;
-  margin: 5px;
-  transition: 0.4s;
+    display: block;
+    width: 30px;
+    height: 3px;
+    background: black;
+    margin: 5px;
+    transition: 0.4s;
 }
 .menu {
-  display: none;
+    display: none;
 }
 .menu ul {
-  display: flex;
-  flex-direction: column;
-  list-style: none;
-  padding: 0;
-  margin: 0;
-  background: white;
-  position: absolute;
-  top: 100%;
-  left: 0;
-  width: 100%;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    display: flex;
+    flex-direction: column;
+    list-style: none;
+    padding: 0;
+    margin: 0;
+    background: white;
+    position: absolute;
+    top: 100%;
+    left: 0;
+    width: 100%;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
 }
 @media (max-width: 1080px) {
-  .button-container {
-    display: none;
-  }
-  .Navbar {
-    justify-content: space-between;
-    position: sticky;
-    top: 0px;
-    background: white;
-    z-index: 100;
-  }
-  .burger-icon {
-    display: block;
-  }
+    .button-container {
+        display: none;
+    }
+    .Navbar {
+        justify-content: space-between;
+        position: sticky;
+        top: 0px;
+        background: white;
+        z-index: 100;
+    }
+    .burger-icon {
+        display: block;
+    }
 }
 </style>
 <script>
+import Menu from "../components/Menu";
 export default {
-  name: "NavBarComponent",
-  methods: {
-    showComponent(componentName) {
-      this.$emit("showComponent", componentName);
+    name: "NavBarComponent",
+    methods: {
+        showComponent(componentName) {
+            this.$emit("showComponent", componentName);
+        },
+        showMenu() {
+            this.menuDisplay = true;
+        },
+        closeMenu() {
+            this.menuDisplay = false;
+        },
     },
-  },
-  data() {
-    return {
-      logoUrl: "/assets/LogoPortofolio.svg",
-      menu: false,
-    };
-  },
+    data() {
+        return {
+            logoUrl: "/assets/LogoPortofolio.svg",
+            menu: false,
+            menuDisplay: false,
+        };
+    },
 };
 </script>
