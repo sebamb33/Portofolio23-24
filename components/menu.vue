5729e69ddbd649547fb39e35571a1f46e3eba2ee
Sébastien33am <sebastien.ambona@agence-gw.com>
2024-10-26 17:48:01 +0200

    Add functionality menu + work on css for technology.vue

---
+++ components/menu.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="menu_nav">
+        <div class="menu_link">
+            <NuxtLink to="/technology">Technologies</NuxtLink>
+        </div>
+        <div class="menu_link">
+            <NuxtLink to="/experience">Experience</NuxtLink>
+        </div>
+    </div>
+    <div class="menu_logo">
+        <a
+            href="https://www.linkedin.com/in/s%C3%A9bastien-ambona-6b1b3a1b0/"
+            target="_blank"
+        >
+            <NuxtLink to="/">
+                <img :src="logoUrl" alt="Logo" width="70" height="70" />
+            </NuxtLink>
+            <img src="https://img.icons8.com/50/000000/linkedin.png" />
+        </a>
+        <a href="https://github.com/sebamb33" target="_blank">
+            <img src="https://img.icons8.com/50/000000/github--v1.png" />
+        </a>
+    </div>
+</template>
+<script lang="ts">
+export default {
+    data() {
+        return {
+            logoUrl: "/assets/LogoPortofolio.svg",
+        };
+    },
+};
+</script>
--- pages/technology.vue
+++ pages/technology.vue
@@ -1,265 +1,290 @@
 <template>
-  <div class="technology">
-    <div class="oneTechno">
-      <div class="technoData">
-        <div class="technologyHeader">
-          <div class="technologyLogo">
-            <Icon name="uil:vuejs" color="black" class="technologyIcon" />
-          </div>
-          <div class="technologyName">VueJs</div>
-        </div>
+    <div class="technology">
+        <div class="oneTechno">
+            <div class="technoData">
+                <div class="technologyHeader">
+                    <div class="technologyLogo">
+                        <Icon
+                            name="uil:vuejs"
+                            color="black"
+                            class="technologyIcon"
+                        />
+                    </div>
+                    <div class="technologyName">VueJs</div>
+                </div>
 
-        <div class="skill">Niveaux :</div>
-        <div class="skillLevel">
-          <Skillbar :skillNumber="4" />
+                <div class="skill">Niveaux :</div>
+                <div class="skillLevel">
+                    <Skillbar :skillNumber="4" />
+                </div>
+                <p class="technologyDetails">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+                    alias aperiam commodi
+                </p>
+            </div>
         </div>
-        <p class="technologyDetails">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias
-          aperiam commodi
-        </p>
-      </div>
-    </div>
-    <div class="oneTechno">
-      <div class="technoData">
-        <div class="technologyHeader">
-          <div class="technologyLogo">
-            <Icon name="uil:react" color="black" class="technologyIcon" />
-          </div>
-          <div class="technologyName">React</div>
-        </div>
+        <div class="oneTechno">
+            <div class="technoData">
+                <div class="technologyHeader">
+                    <div class="technologyLogo">
+                        <Icon
+                            name="uil:react"
+                            color="black"
+                            class="technologyIcon"
+                        />
+                    </div>
+                    <div class="technologyName">React</div>
+                </div>
 
-        <div class="skill">Niveaux :</div>
-        <div class="skillLevel">
-          <Skillbar :skillNumber="3" />
+                <div class="skill">Niveaux :</div>
+                <div class="skillLevel">
+                    <Skillbar :skillNumber="3" />
+                </div>
+                <p class="technologyDetails">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+                    alias aperiam commodi
+                </p>
+            </div>
         </div>
-        <p class="technologyDetails">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias
-          aperiam commodi
-        </p>
-      </div>
-    </div>
-    <div class="oneTechno">
-      <div class="technoData">
-        <div class="technologyHeader">
-          <div class="technologyLogo">
-            <Icon name="devicon:express" color="black" class="technologyIcon" />
-          </div>
-          <div class="technologyName">Express</div>
-        </div>
+        <div class="oneTechno">
+            <div class="technoData">
+                <div class="technologyHeader">
+                    <div class="technologyLogo">
+                        <Icon
+                            name="devicon:express"
+                            color="black"
+                            class="technologyIcon"
+                        />
+                    </div>
+                    <div class="technologyName">Express</div>
+                </div>
 
-        <div class="skill">Niveaux :</div>
-        <div class="skillLevel">
-          <Skillbar :skillNumber="3" />
+                <div class="skill">Niveaux :</div>
+                <div class="skillLevel">
+                    <Skillbar :skillNumber="3" />
+                </div>
+                <p class="technologyDetails">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+                    alias aperiam commodi
+                </p>
+            </div>
         </div>
-        <p class="technologyDetails">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias
-          aperiam commodi
-        </p>
-      </div>
-    </div>
-    <div class="oneTechno">
-      <div class="technoData">
-        <div class="technologyHeader">
-          <div class="technologyLogo">
-            <Icon
-              name="tabler:brand-prisma"
-              color="black"
-              class="technologyIcon"
-            />
-          </div>
-          <div class="technologyName">Prisma</div>
-        </div>
+        <div class="oneTechno">
+            <div class="technoData">
+                <div class="technologyHeader">
+                    <div class="technologyLogo">
+                        <Icon
+                            name="tabler:brand-prisma"
+                            color="black"
+                            class="technologyIcon"
+                        />
+                    </div>
+                    <div class="technologyName">Prisma</div>
+                </div>
 
-        <div class="skill">Niveaux :</div>
-        <div class="skillLevel">
-          <Skillbar :skillNumber="3" />
+                <div class="skill">Niveaux :</div>
+                <div class="skillLevel">
+                    <Skillbar :skillNumber="3" />
+                </div>
+                <p class="technologyDetails">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+                    alias aperiam commodi
+                </p>
+            </div>
         </div>
-        <p class="technologyDetails">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias
-          aperiam commodi
-        </p>
-      </div>
-    </div>
-    <div class="oneTechno">
-      <div class="technoData">
-        <div class="technologyHeader">
-          <div class="technologyLogo">
-            <Icon
-              name="simple-icons:jest"
-              color="black"
-              class="technologyIcon"
-            />
-          </div>
-          <div class="technologyName">Jest</div>
-        </div>
+        <div class="oneTechno">
+            <div class="technoData">
+                <div class="technologyHeader">
+                    <div class="technologyLogo">
+                        <Icon
+                            name="simple-icons:jest"
+                            color="black"
+                            class="technologyIcon"
+                        />
+                    </div>
+                    <div class="technologyName">Jest</div>
+                </div>
 
-        <div class="skill">Niveaux :</div>
-        <div class="skillLevel">
-          <Skillbar :skillNumber="5" />
+                <div class="skill">Niveaux :</div>
+                <div class="skillLevel">
+                    <Skillbar :skillNumber="5" />
+                </div>
+                <p class="technologyDetails">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+                    alias aperiam commodi
+                </p>
+            </div>
         </div>
-        <p class="technologyDetails">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias
-          aperiam commodi
-        </p>
-      </div>
-    </div>
-    <div class="oneTechno">
-      <div class="technoData">
-        <div class="technologyHeader">
-          <div class="technologyLogo">
-            <Icon name="logos:symfony" color="black" class="technologyIcon" />
-          </div>
-          <div class="technologyName">Symfony</div>
-        </div>
+        <div class="oneTechno">
+            <div class="technoData">
+                <div class="technologyHeader">
+                    <div class="technologyLogo">
+                        <Icon
+                            name="logos:symfony"
+                            color="black"
+                            class="technologyIcon"
+                        />
+                    </div>
+                    <div class="technologyName">Symfony</div>
+                </div>
 
-        <div class="skill">Niveaux :</div>
-        <div class="skillLevel">
-          <Skillbar :skillNumber="2" />
+                <div class="skill">Niveaux :</div>
+                <div class="skillLevel">
+                    <Skillbar :skillNumber="2" />
+                </div>
+                <p class="technologyDetails">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+                    alias aperiam commodi
+                </p>
+            </div>
         </div>
-        <p class="technologyDetails">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias
-          aperiam commodi
-        </p>
-      </div>
-    </div>
-    <div class="oneTechno">
-      <div class="technoData">
-        <div class="technologyHeader">
-          <div class="technologyLogo">
-            <Icon
-              name="devicon-plain:cypressio-wordmark"
-              color="black"
-              class="technologyIcon"
-            />
-          </div>
-          <div class="technologyName">Cypress</div>
-        </div>
+        <div class="oneTechno">
+            <div class="technoData">
+                <div class="technologyHeader">
+                    <div class="technologyLogo">
+                        <Icon
+                            name="devicon-plain:cypressio-wordmark"
+                            color="black"
+                            class="technologyIcon"
+                        />
+                    </div>
+                    <div class="technologyName">Cypress</div>
+                </div>
 
-        <div class="skill">Niveaux :</div>
-        <div class="skillLevel">
-          <Skillbar :skillNumber="4" />
+                <div class="skill">Niveaux :</div>
+                <div class="skillLevel">
+                    <Skillbar :skillNumber="4" />
+                </div>
+                <p class="technologyDetails">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+                    alias aperiam commodi
+                </p>
+            </div>
         </div>
-        <p class="technologyDetails">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias
-          aperiam commodi
-        </p>
-      </div>
-    </div>
-    <div class="oneTechno">
-      <div class="technoData">
-        <div class="technologyHeader">
-          <div class="technologyLogo">
-            <Icon name="la:docker" color="black" class="technologyIcon" />
-          </div>
-          <div class="technologyName">Docker</div>
-        </div>
+        <div class="oneTechno">
+            <div class="technoData">
+                <div class="technologyHeader">
+                    <div class="technologyLogo">
+                        <Icon
+                            name="la:docker"
+                            color="black"
+                            class="technologyIcon"
+                        />
+                    </div>
+                    <div class="technologyName">Docker</div>
+                </div>
 
-        <div class="skill">Niveaux :</div>
-        <div class="skillLevel">
-          <Skillbar :skillNumber="4" />
+                <div class="skill">Niveaux :</div>
+                <div class="skillLevel">
+                    <Skillbar :skillNumber="4" />
+                </div>
+                <p class="technologyDetails">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+                    alias aperiam commodi
+                </p>
+            </div>
         </div>
-        <p class="technologyDetails">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias
-          aperiam commodi
-        </p>
-      </div>
-    </div>
-    <div class="oneTechno">
-      <div class="technoData">
-        <div class="technologyHeader">
-          <div class="technologyLogo">
-            <Icon
-              name="logos:kirby-icon"
-              color="black"
-              class="technologyIcon"
-            />
-          </div>
-          <div class="technologyName">Kirby</div>
-        </div>
+        <div class="oneTechno">
+            <div class="technoData">
+                <div class="technologyHeader">
+                    <div class="technologyLogo">
+                        <Icon
+                            name="logos:kirby-icon"
+                            color="black"
+                            class="technologyIcon"
+                        />
+                    </div>
+                    <div class="technologyName">Kirby</div>
+                </div>
 
-        <div class="skill">Niveaux :</div>
-        <div class="skillLevel">
-          <Skillbar :skillNumber="4" />
+                <div class="skill">Niveaux :</div>
+                <div class="skillLevel">
+                    <Skillbar :skillNumber="4" />
+                </div>
+                <p class="technologyDetails">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+                    alias aperiam commodi
+                </p>
+            </div>
         </div>
-        <p class="technologyDetails">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad alias
-          aperiam commodi
-        </p>
-      </div>
     </div>
-  </div>
 </template>
 <script lang="ts">
 import Skillbar from "~/components/Skillbar.vue";
 
 export default {
-  components: { Skillbar },
-  mounted() {
-    const technologies = this.$el.querySelectorAll(".oneTechno");
-    technologies.forEach((techno, index) => {
-      techno.style.animationDelay = `${index * 0.3}s`;
-    });
-  },
+    components: { Skillbar },
+    mounted() {
+        const technologies = this.$el.querySelectorAll(".oneTechno");
+        technologies.forEach((techno, index) => {
+            techno.style.animationDelay = `${index * 0.3}s`;
+        });
+    },
 };
 </script>
 <style scoped>
 .technology {
-  padding: 2rem;
-  height: 80vh;
-  overflow-y: scroll;
-  flex-wrap: wrap;
-  display: flex;
-  flex-direction: row;
-  justify-content: space-between;
-  width: 80%;
-  margin: auto;
-  -ms-overflow-style: none;
+    padding: 2rem;
+    height: 80vh;
+    overflow-y: scroll;
+    flex-wrap: wrap;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    width: 80%;
+    margin: auto;
+    -ms-overflow-style: none;
 }
 .technology::-webkit-scrollbar {
-  display: none;
+    display: none;
 }
 .technologyHeader {
-  margin: 3px;
-  display: flex;
-  align-items: flex-end;
-  border-bottom: 2px solid black;
-  padding-bottom: 1rem;
+    margin: 3px;
+    display: flex;
+    align-items: flex-end;
+    border-bottom: 2px solid black;
+    padding-bottom: 1rem;
 }
 .technologyName {
-  font-size: 2rem;
-  margin-left: 1rem;
+    font-size: 2rem;
+    margin-left: 1rem;
 }
 .skill {
-  margin: 1rem 0rem;
+    margin: 1rem 0rem;
 }
 .oneTechno {
-  display: flex;
-  flex-direction: column;
-  max-width: 20rem;
-  height: 40%;
-  margin-bottom: 5rem;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  transition: transform 0.5s ease;
-  animation: slideIn 450ms ease-out forwards;
-  transform: translateY(200px);
-  filter: blur(10px);
-  border-radius: 3px;
+    display: flex;
+    flex-direction: column;
+    max-width: 20rem;
+    height: 40%;
+    margin-bottom: 5rem;
+    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+    transition: transform 0.5s ease;
+    animation: slideIn 450ms ease-out forwards;
+    transform: translateY(200px);
+    filter: blur(10px);
+    border-radius: 3px;
 }
 .oneTechno:hover {
-  transform: scale(1.2) !important;
+    transform: scale(1.2) !important;
 }
 .technoData {
-  margin: 1rem;
+    margin: 1rem;
 }
 .technologyIcon {
-  height: 3rem;
-  width: 3rem;
+    height: 3rem;
+    width: 3rem;
 }
 @keyframes slideIn {
-  to {
-    opacity: 1;
-    transform: translateY(0);
-    filter: blur(0);
-  }
+    to {
+        opacity: 1;
+        transform: translateY(0);
+        filter: blur(0);
+    }
+}
+@media (max-width: 815px) {
+    .technology {
+        justify-content: center;
+    }
 }
 </style>
