5dce502dae20ab3a85fab4414e04f33b8b7b4ba2
Sébastien33am <sebastien.ambona@agence-gw.com>
2024-10-26 19:43:09 +0200

    Add css for transition when the user use menu for mobile

--- components/menu.vue
+++ components/menu.vue
@@ -1,5 +1,9 @@
 <template>
-    <div class="menu_nav" v-if="menuOpen">
+    <div
+        class="menu_nav"
+        :class="{ 'slide-in': menuOpen }"
+        @animationend="onMenuAnimationEnd"
+    >
         <div class="menu_top" @click="closeMenu">
             <img
                 src="https://img.icons8.com/ios-glyphs/30/000000/delete-sign.png"
@@ -7,16 +11,15 @@
             />
         </div>
         <div class="menu_router">
-            <div class="menu_link">
-                <NuxtLink to="/" @click="closeMenu"> Accueil </NuxtLink>
-            </div>
-            <div class="menu_link">
-                <NuxtLink to="/technology" @click="closeMenu"
-                    >Technologies</NuxtLink
-                >
-            </div>
-            <div class="menu_link" @click="closeMenu">
-                <NuxtLink to="/experience">Experience</NuxtLink>
+            <div
+                class="menu_link"
+                v-for="(link, index) in links"
+                :key="index"
+                :class="{ 'slide-in-link': menuOpen && linksAnimated }"
+            >
+                <NuxtLink :to="link.to" @click="closeMenu">
+                    {{ link.label }}
+                </NuxtLink>
             </div>
         </div>
         <div class="menu_logo">
@@ -32,23 +35,38 @@
         </div>
     </div>
 </template>
+
 <script lang="ts">
 export default {
     data() {
         return {
             logoUrl: "/assets/LogoPortofolio.svg",
             menuOpen: true,
+            linksAnimated: false,
+            links: [
+                { to: "/", label: "Accueil" },
+                { to: "/technology", label: "Technologies" },
+                { to: "/experience", label: "Experience" },
+            ],
         };
     },
     methods: {
         closeMenu() {
             this.menuOpen = false;
+            this.linksAnimated = false;
             this.$emit("closeMenu");
+        },
+        onMenuAnimationEnd() {
+            // Déclenche l'animation des liens après la fin de l'animation du menu
+            if (this.menuOpen) {
+                this.linksAnimated = true;
+            }
         },
     },
 };
 </script>
-<style>
+
+<style scoped>
 .menu_nav {
     height: 100vh;
     width: 100%;
@@ -61,7 +79,62 @@ export default {
     left: 0px;
     top: 0px;
     background: white;
+    transform: translateX(-100%);
+    animation: slide-out 0.3s forwards; /* Animation pour la fermeture */
 }
+
+.slide-in {
+    animation: slide-in 0.3s forwards; /* Animation pour l'ouverture */
+}
+
+@keyframes slide-in {
+    from {
+        transform: translateX(-100%);
+    }
+    to {
+        transform: translateX(0);
+    }
+}
+
+@keyframes slide-out {
+    from {
+        transform: translateX(0);
+    }
+    to {
+        transform: translateX(-100%);
+    }
+}
+
+.menu_router {
+    height: 80vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+.menu_link {
+    opacity: 0;
+    padding-bottom: 20px;
+    font-size: 50px;
+    font-weight: 800;
+    transform: translateX(-50px);
+    transition:
+        transform 0.3s ease-out,
+        opacity 0.3s ease-out;
+}
+
+.slide-in-link {
+    opacity: 1;
+    transform: translateX(0);
+    transition-delay: 0.1s; /* Délai pour l'apparition des liens */
+}
+
+.menu_link a {
+    text-decoration: none;
+    color: black;
+    font-family: "Fira Code", monospace;
+}
+
 .menu_top {
     display: flex;
     justify-content: flex-end;
@@ -69,19 +142,4 @@ export default {
     margin-bottom: 50px;
     align-items: center;
 }
-
-.menu_router {
-    height: 80vh;
-}
-.menu_link {
-    padding-bottom: 20px;
-    font-size: 50px;
-    font-weight: 800;
-}
-.menu_link a {
-    text-decoration: none;
-    color: black;
-    font-family: "Fira Code", monospace;
-}
 </style>
-```
