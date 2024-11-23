50464ba33812baa3f406ea98f9bd461861b96833
root <root@ov-6ddb28.infomaniak.ch>
2024-11-23 15:54:16 +0000

    update for server upload

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,10 @@
 
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['nuxt-icon']
+  modules: ['nuxt-icon'],
+  
+  // Configuration du serveur pour changer le port
+  server: {
+    port: 3001 // Définit le port à 3001
+  }
 })
